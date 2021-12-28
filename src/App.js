95dd96137d5827e37f4b55e8a983cc28dc0af8ac
MasahiroKaga <masahiro41802891@gmail.com>
2021-12-28 15:49:44 -0800

    Section20 starting point

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { Fragment } from 'react';
-
-import Counter from './components/Counter';
-import Headers from './components/Header';
-import Auth from './components/Auth';
-
-
-
 function App() {
   return (
-    <Fragment>
-      <Headers />
-      <Auth />
-      <Counter />
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
