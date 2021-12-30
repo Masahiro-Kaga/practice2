16f40450621e4c219ff00d98b1e5d650103b61fd
MasahiroKaga <masahiro41802891@gmail.com>
2021-12-30 15:09:05 -0800

    complete till 276, before practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+import ProductDetail from "./pages/ProductDetail";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader></MainHeader>
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome'></Redirect>
+          </Route>
+          <Route path="/welcome">
+            <Welcome></Welcome>
+          </Route>
+          <Route path="/products" exact>
+            <Products></Products>
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail></ProductDetail>
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
