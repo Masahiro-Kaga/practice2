aef43861c5c646b3787444ffb5d5ff428a065f6a
Masahiro Kaga <masahiro41802891@gmail.com>
2021-12-30 17:33:05 -0800

    starting point of practice

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import MainHeader from "./components/MainHeader";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
-import ProductDetail from "./pages/ProductDetail";
-
 function App() {
   return (
     <div>
-      <MainHeader></MainHeader>
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome'></Redirect>
-          </Route>
-          <Route path="/welcome">
-            <Welcome></Welcome>
-          </Route>
-          <Route path="/products" exact>
-            <Products></Products>
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail></ProductDetail>
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
 }
