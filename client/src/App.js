6e4e7467750101a7bb9d3ddb38767200221a0745
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-02 00:31:14 -0800

    Fail to convert from axios to fetch I think because Asyncness process

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,54 @@ import Axios from "axios";
 
 function App() {
   const [listOfUsers, setListOfUsers] = useState([]);
-  const [name ,setName] = useState("")
-  const [age ,setAge] = useState("")
-  const [username ,setUsername] = useState("")
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [username, setUsername] = useState("");
+
+
+  // Try to use fetch (fail/error) -------------------------------------------
+
+  // const sendHttpRequest = (method, url, data) => {
+  //   return fetch(url, {
+  //     method: method,
+  //     data: JSON.stringify(data),
+  //     headers: data ? { "Content-Type": "application/json" } : {},
+  //   }).then((response) => {
+  //     if (response.status >= 400) {
+  //       return response.json().then((errorResponseData) => {
+  //         const error = new Error("Something wrong!");
+  //         error.data = errorResponseData;
+  //         throw error;
+  //       });
+  //     }
+  //     return response.json();
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   sendHttpRequest("GET", "http://localhost:4000/getUsers").then(
+  //     (response) => {
+  //       setListOfUsers(response.data);
+  //     }
+  //   );
+  // }, []);
+
+  // const createUser = () => {
+  //   sendHttpRequest("POST", "http://localhost:4000/createUser", {
+  //     name,
+  //     age,
+  //     username,
+  //   }).then((response) => {
+  //     setListOfUsers([...listOfUsers], {
+  //       name,
+  //       age,
+  //       username,
+  //     });
+  //   });
+  // };
+
+
+  // Try to use Axios (success) -------------------------------------------
 
   useEffect(() => {
     Axios.get("http://localhost:4000/getUsers").then((response) => {
@@ -19,20 +64,41 @@ function App() {
       age,
       username,
     }).then((response) => {
-      setListOfUsers([...listOfUsers,{
-        name,
-        age,
-        username,
-      }]);
+      setListOfUsers([
+        ...listOfUsers,
+        {
+          name,
+          age,
+          username,
+        },
+      ]);
     });
   };
 
   return (
     <div>
       <div>
-        <input type="text" placeholder="name..." onChange={(event)=>{setName(event.target.value)}}/>
-        <input type="text" placeholder="age..." onChange={(event)=>{setAge(event.target.value)}}/>
-        <input type="text" placeholder="username..." onChange={(event)=>{setUsername(event.target.value)}}/>
+        <input
+          type="text"
+          placeholder="name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
         <button onClick={createUser}>Create User</button>
       </div>
       {listOfUsers.map((user, index) => {
@@ -45,7 +111,6 @@ function App() {
           </div>
         );
       })}
-
     </div>
   );
 }
